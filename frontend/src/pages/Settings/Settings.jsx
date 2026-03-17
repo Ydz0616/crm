cff60c1630d6c03d75047deec6d0498c6a6f6f79
willziheng <will.ziheng@outlook.com>
2026-03-17 18:28:50 +0800

    ui: about copy to settings

--- frontend/src/pages/Settings/Settings.jsx
+++ frontend/src/pages/Settings/Settings.jsx
@@ -5,6 +5,7 @@ import {
   FileImageOutlined,
   TrophyOutlined,
 } from '@ant-design/icons';
+import { Button, Result } from 'antd'; // Imported from antd
 
 import TabsContent from '@/components/TabsContent/TabsContent';
 
@@ -55,5 +56,35 @@ export default function Settings() {
 
   const pageTitle = translate('Settings');
 
-  return <TabsContent defaultActiveKey={settingsKey} content={content} pageTitle={pageTitle} />;
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '40px', paddingBottom: '40px' }}>
+      <TabsContent defaultActiveKey={settingsKey} content={content} pageTitle={pageTitle} />
+      <Result
+        status="info"
+        title={'Ola ERP CRM'}
+        subTitle={translate('Contact us if you need extra help')}
+        extra={
+          <>
+            <p>
+              Website : <a href="https://www.olajob.cn">www.olajob.cn</a>{' '}
+            </p>
+            <p>
+              GitHub :{' '}
+              <a href="https://github.com/Ydz0616/crm">
+                https://github.com/ydz0616/crm
+              </a>
+            </p>
+            <Button
+              type="primary"
+              onClick={() => {
+                window.open(`https://www.olajob.cn`);
+              }}
+            >
+              {translate('Contact us')}
+            </Button>
+          </>
+        }
+      />
+    </div>
+  );
 }
