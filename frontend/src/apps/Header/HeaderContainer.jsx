5297e3fe522a3caba991a1b443df6e31e2b595d4
willziheng <will.ziheng@outlook.com>
2026-03-18 09:46:11 +0800

    ui: delete header of all page

--- frontend/src/apps/Header/HeaderContainer.jsx
+++ frontend/src/apps/Header/HeaderContainer.jsx
@@ -1,79 +1,13 @@
-import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { Avatar, Dropdown, Layout, Input } from 'antd';
+import { Layout, Input } from 'antd';
 
-import { LogoutOutlined, ToolOutlined, UserOutlined, SearchOutlined } from '@ant-design/icons';
-
-import { selectCurrentAdmin } from '@/redux/auth/selectors';
-
-import { FILE_BASE_URL } from '@/config/serverApiConfig';
+import { SearchOutlined } from '@ant-design/icons';
 
 import useLanguage from '@/locale/useLanguage';
 
 export default function HeaderContent() {
-  const currentAdmin = useSelector(selectCurrentAdmin);
   const { Header } = Layout;
 
   const translate = useLanguage();
-
-  const ProfileDropdown = () => {
-    const navigate = useNavigate();
-    return (
-      <div className="profileDropdown" onClick={() => navigate('/profile')}>
-        <Avatar
-          size="large"
-          className="last"
-          src={currentAdmin?.photo ? FILE_BASE_URL + currentAdmin?.photo : undefined}
-          style={{
-            color: '#f56a00',
-            backgroundColor: currentAdmin?.photo ? 'none' : '#fde3cf',
-          }}
-        >
-          {currentAdmin?.name?.charAt(0)?.toUpperCase()}
-        </Avatar>
-        <div className="profileDropdownInfo">
-          <p>
-            {currentAdmin?.name} {currentAdmin?.surname}
-          </p>
-          <p>{currentAdmin?.email}</p>
-        </div>
-      </div>
-    );
-  };
-
-  const items = [
-    {
-      label: <ProfileDropdown className="headerDropDownMenu" />,
-      key: 'ProfileDropdown',
-    },
-    {
-      type: 'divider',
-    },
-    {
-      icon: <UserOutlined />,
-      key: 'settingProfile',
-      label: (
-        <Link to={'/profile'}>
-          {translate('profile_settings')}
-        </Link>
-      ),
-    },
-    {
-      icon: <ToolOutlined />,
-      key: 'settingApp',
-      label: <Link to={'/settings'}>{translate('app_settings')}</Link>,
-    },
-
-    {
-      type: 'divider',
-    },
-
-    {
-      icon: <LogoutOutlined />,
-      key: 'logout',
-      label: <Link to={'/logout'}>{translate('logout')}</Link>,
-    },
-  ];
 
   return (
     <Header
@@ -97,27 +31,6 @@ export default function HeaderContent() {
           }}
         />
       </div>
-
-      <Dropdown
-        menu={{
-          items,
-        }}
-        trigger={['click']}
-        placement="bottomRight"
-      >
-        <Avatar
-          className="last"
-          src={currentAdmin?.photo ? FILE_BASE_URL + currentAdmin?.photo : undefined}
-          style={{
-            color: '#f56a00',
-            backgroundColor: currentAdmin?.photo ? 'none' : '#fde3cf',
-            cursor: 'pointer',
-          }}
-          size="default"
-        >
-          {currentAdmin?.name?.charAt(0)?.toUpperCase()}
-        </Avatar>
-      </Dropdown>
     </Header>
   );
 }
