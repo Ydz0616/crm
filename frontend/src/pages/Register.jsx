6ef862214dd7a4c5b2301d3c37a8262c167b6694
Yuandong Zhang <107053257+Ydz0616@users.noreply.github.com>
2026-03-28 22:44:11 -0700

    feat(auth-ui): implement frontend registration workflow
    - Created Register and RegisterForm components adhering to AntD styles
    - Hooked /register route into AuthRouter
    - Linked Login page to Register flow
    - Connected existing Redux register action with frontend navigation

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,33 +1,40 @@
 import { useEffect } from 'react';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 
 import useLanguage from '@/locale/useLanguage';
+import { Form, Button, Typography, notification } from 'antd';
 
-import { Form, Button, Typography } from 'antd';
-
-const { Text } = Typography;
-
-import { login } from '@/redux/auth/actions';
+import { register } from '@/redux/auth/actions';
 import { selectAuth } from '@/redux/auth/selectors';
-import LoginForm from '@/forms/LoginForm';
+import RegisterForm from '@/forms/RegisterForm';
 import Loading from '@/components/Loading';
 import AuthModule from '@/modules/AuthModule';
 
-const LoginPage = () => {
+const { Text } = Typography;
+
+const RegisterPage = () => {
   const translate = useLanguage();
   const { isLoading, isSuccess } = useSelector(selectAuth);
   const navigate = useNavigate();
-  // const size = useSize();
-
   const dispatch = useDispatch();
+
   const onFinish = (values) => {
-    dispatch(login({ loginData: values }));
+    // 剔除 confirmPassword, 因为后端 API 只需要 name, email, password
+    const { confirmPassword, ...registerData } = values;
+    dispatch(register({ registerData }));
   };
 
   useEffect(() => {
-    if (isSuccess) navigate('/');
+    if (isSuccess) {
+      // 当 register success 返回时
+      notification.success({
+        message: 'Registration Successful',
+        description: 'You can now log in with your new credentials.',
+      });
+      // 成功后自动跳转到 Login 页面强制用户输入密码以获取包含身份令牌的 Context
+      navigate('/login');
+    }
   }, [isSuccess]);
 
   const FormContainer = () => {
@@ -35,14 +42,12 @@ const LoginPage = () => {
       <Loading isLoading={isLoading}>
         <Form
           layout="vertical"
-          name="normal_login"
+          name="register_form"
           className="login-form"
-          initialValues={{
-            remember: true,
-          }}
           onFinish={onFinish}
         >
-          <LoginForm />
+          <RegisterForm />
+          
           <Form.Item>
             <Button
               type="primary"
@@ -51,20 +56,20 @@ const LoginPage = () => {
               loading={isLoading}
               size="large"
             >
-              {translate('Log in')}
+              Sign up
             </Button>
           </Form.Item>
-
+          
           <div style={{ textAlign: 'center', marginTop: 10 }}>
-            <Text>Don't have an account? </Text>
-            <Link to="/register">Sign up</Link>
+            <Text>Already have an account? </Text>
+            <Link to="/login">Log in</Link>
           </div>
         </Form>
       </Loading>
     );
   };
 
-  return <AuthModule authContent={<FormContainer />} AUTH_TITLE="Sign in" />;
+  return <AuthModule authContent={<FormContainer />} AUTH_TITLE="Sign up" />;
 };
 
-export default LoginPage;
+export default RegisterPage;
